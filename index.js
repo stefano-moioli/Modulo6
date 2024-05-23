4ae90bf34010a45a24e1add128f3250214d3add4
stefano-moioli <151653556+stefano-moioli@users.noreply.github.com>
2024-05-23 18:04:59 +0200

    initServer

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express'); //Importo express
+const cors = require ('cors');
+const mongoose = require ('mongoose');
+const app = express(); //Lo eseguo
+const port = 3001;
+
+//Middleware
+app.use(cors());
+app.use(express.json()); //Trasforma in json l'oggetto - senza fare json stringify o json parse
+
+async function start(){
+    try {
+        await mongoose.connect('mongodb+srv://StefanoM:1r8U0SOZDVAt00V4@cluster0.csj3q0k.mongodb.net/');
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`); //Lo accendo
+        })
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+start()
+
+ // mongoose.connect('mongodb+srv://StefanoM:1r8U0SOZDVAt00V4@cluster0.csj3q0k.mongodb.net/'); //Oggetto capace di connettere le due cose (database e server - credo)
+
+
+app.get('/home', (req, res) =>{    //Endpoint che verrà richiamato dal client
+    res.send('Ciao, benvenuto')
+}) 
+
+/* app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`); //Lo accendo
+})*/
+
